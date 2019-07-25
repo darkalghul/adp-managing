c4dcf592c71afad45353c416bc9ca47c9e920642
Gabriel Gonzalez <hgglez92@gmail.com>
2019-07-24 17:15:45 -0700

    section two advance

--- js/main.js
+++ js/main.js
@@ -1,43 +1,39 @@
-$(document).ready(function() {
-    var counters = $(".counter");
-    var countersQuantity = counters.length;
-    var counter = [];
-  
-    for (i = 0; i < countersQuantity; i++) {
-        counter[i] = parseInt(counters[i].innerHTML);
+let a = 0;
+
+$(window).scroll(function () {
+    var oTop = $('.numbers').offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() > oTop) {
+        $('.counter').each(function () {
+            var $this = $(this),
+                countTo = $this.attr('data-count');
+            $({
+                countNum: $this.text()
+            }).animate({
+                    countNum: countTo
+                },
+                {
+
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function () {
+                        $this.text(Math.floor(this.countNum) + '%');
+                    },
+                    complete: function () {
+                        $this.text(this.countNum + '%');
+                    }
+
+                });
+        });
+        a = 1;
     }
-  
-    var count = function(start, value, id) {
-        var localStart = start;
-        setInterval(function() {
-            if (localStart < value) {
-                localStart++;
-                counters[id].innerHTML = localStart + "%";
-            }
-        }, 100);
-    }
-  
-    for (j = 0; j < countersQuantity; j++) {
-        count(0, counter[j], j);
-    }
-
-    // $svg = $('svg').drawsvg({
-    //     callback: function() {
-    //         animate();
-    //     }
-    // });
-
-    // function animate() {
-    //     $svg.drawsvg('animate');  
-    // }
-
-    // animate();
-
-    var $calculator = $('#calculator').drawsvg();
-    var $lightbulb = $('#lightbulb').drawsvg();
-    $calculator.drawsvg('animate');
-    $lightbulb.drawsvg('animate');
 });
+
+// $(document).ready(function() {
+//     var $calculator = $('#calculator').drawsvg();
+//     var $lightbulb = $('#lightbulb').drawsvg();
+//     $calculator.drawsvg('animate');
+//     $lightbulb.drawsvg('animate');
+// });
 
 // var $doc = $(document),
 //     $wind = $(window),
